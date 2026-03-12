fa47859aadeb2e28eebda472a508ab95164e1b9b
Adambezzzz <adamgbezerra@gmail.com>
2026-03-12 19:11:11 +0000

    feat: implementar moldura raiz

---
+++ src/app/layout.tsx
@@ -0,0 +1,17 @@
+import type {Metadata} from 'next';
+import { inter } from '@/app/ui/fonts';
+import '@app/global.css';
+
+export const metadata: Metadata  = {
+title: 'ACNE'
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return(
+    <html lang='pt-br'>
+      <body className={ inter.className } >
+        { children }
+      </body>
+    </html>
+  );
+};
